b16e0e40544599b8f04f100f71224e48448834bd
joaopedroa <joaopedroa.6@hotmail.com>
2021-03-27 20:48:47 -0300

    Inclusão do ms sgm-georeferenciamento-service

---
+++ georreferenciamento/georreferenciamento/src/main/java/br/com/bomdestino/sgm/georreferenciamento/GeorreferenciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bomdestino.sgm.georreferenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorreferenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorreferenciamentoApplication.class, args);
+	}
+
+}
